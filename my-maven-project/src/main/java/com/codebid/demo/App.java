fcb0c3e4b234c8911696a44240232a4b7e85ecb7
rajinikantherugu <68960115+rajinikantherugu@users.noreply.github.com>
2020-09-09 16:17:16 +0530

    maven commit

---
+++ my-maven-project/src/main/java/com/codebid/demo/App.java
@@ -0,0 +1,13 @@
+package com.codebid.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
